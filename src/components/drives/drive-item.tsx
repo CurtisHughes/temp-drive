c4e2433082a0b86e27987ce86344d6d98b408970
Curtis Hughes <curtis@curtiss-mbp.attlocal.net>
2021-11-18 23:14:14 -0500

    feat: delete button

--- src/components/drives/drive-item.tsx
+++ src/components/drives/drive-item.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
-import { Flex, Progress, Icon, IconButton, Text, chakra } from '@chakra-ui/react';
+import { Flex, Progress, Icon, IconButton, Text, Menu, MenuButton, MenuList, MenuItem, chakra } from '@chakra-ui/react';
 import { FaEllipsisV } from 'react-icons/fa';
 import { FiHardDrive } from 'react-icons/fi';
 import { DateTime } from 'luxon';
 
 import { Drive } from '../../gateways/drives';
 import { calculateTimeLeftInMinutes } from '../../utils/timeLeftInMinutes';
+import { useRemoveDrive } from '../../gateways/drives/hooks/useRemoveDrive';
 
-export const DriveItem: React.FC<{ drive: Drive }> = ({ drive: { createdDateTime, name } }) => {
+export const DriveItem: React.FC<{ drive: Drive }> = ({ drive }) => {
+  const [{ loading }, removeDrive] = useRemoveDrive();
   const [dateTime, setDateTime] = useState(DateTime.local());
-  const timeLeftInMinutes = calculateTimeLeftInMinutes(createdDateTime, dateTime);
+  const timeLeftInMinutes = calculateTimeLeftInMinutes(drive.createdDateTime, dateTime);
 
   useEffect(() => {
     const interval = setInterval(() => setDateTime(DateTime.local()), 1000);
@@ -28,19 +30,32 @@ export const DriveItem: React.FC<{ drive: Drive }> = ({ drive: { createdDateTime
       <chakra.div width="100%" textAlign="left" ml="3">
         <Flex justifyContent="space-between" alignItems="center">
           <chakra.span paddingRight="2">
-            <chakra.header>{name}</chakra.header>
+            <chakra.header>{drive.name}</chakra.header>
             <Text fontSize="xs" color="gray.400" noOfLines={2}>
               {`${Math.round(timeLeftInMinutes)} minute(s) left`}
             </Text>
           </chakra.span>
-          <IconButton
-            aria-label="options"
-            icon={<FaEllipsisV />}
-            onClick={(e) => {
-              e.stopPropagation();
-              console.log('icon');
-            }}
-          />
+          <Menu>
+            <MenuButton
+              onClick={(e) => e.stopPropagation()}
+              as={IconButton}
+              isLoading={loading}
+              aria-label="options"
+              icon={<FaEllipsisV />}
+            >
+              Actions
+            </MenuButton>
+            <MenuList>
+              <MenuItem
+                onClick={(e) => {
+                  e.stopPropagation();
+                  removeDrive(drive);
+                }}
+              >
+                Delete
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </Flex>
         <Progress value={(timeLeftInMinutes / 15) * 100} size="xs" colorScheme="teal" marginTop="4" />
       </chakra.div>
